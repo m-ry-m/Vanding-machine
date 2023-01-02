a0155c0cb68c1f669f8ce9f5123c96257ec45bf0
Maryam <113088482+m-ry-m@users.noreply.github.com>
2023-01-02 21:27:57 +0400

    Create testingloop.py

---
+++ testingloop.py
@@ -0,0 +1,10 @@
+amount=0
+i=0
+while i<=1:
+    k=input('Enter an amount: (Enter quit to leave)')
+    if k=='quit':
+        break
+    else:
+        print("Add 1")
+        amount=amount+1
+print('Amount =',amount)
