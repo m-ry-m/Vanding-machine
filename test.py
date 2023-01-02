c4fa966f7d56b623b8e493f25071e0132fa22908
Maryam <113088482+m-ry-m@users.noreply.github.com>
2023-01-02 21:27:55 +0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+drink=['cola','fanta','pepsi','juice']
+cart=[]
+c=input('Which snack item would you like to add in you cart?')
+cart.append(drink[c])
+print("Its not working")
+print(len(cart))
+print(cart)
