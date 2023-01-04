0cd2790ae32a60345a6e2fcf86d208dba2edf813
Maryam <113088482+m-ry-m@users.noreply.github.com>
2023-01-04 10:37:37 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,27 @@
-drink=['cola','fanta','pepsi','juice']
-cart=[]
-c=input('Which snack item would you like to add in you cart?')
-cart.append(drink[c])
-print("Its not working")
-print(len(cart))
-print(cart)
+amount=50
+csh=int(input("Enter your cash: "))
+
+def perfect_cash(amount,csh):
+    change=csh-amount
+    print("\nYour change is:",change,'Aed')
+def more_cash(amount,csh):
+    print('Please add more cash to purchase:')
+    more=amount-csh
+    print("More amount needed:",more)
+    cas=int(input("Enter more amount: "))
+    csh=csh+cas
+
+#In case if more cash
+if csh>amount:
+    perfect_cash(amount,csh)    
+elif csh<amount:
+    more_cash(amount,csh)
+    if csh>amount:
+        perfect_cash(amount,csh)
+print("Enjoy!!")
+
+
+
+#next user
+#Promotion going on, buy for 10 dhs anything and get an item for half the price
+#A message that tells the user that a particular drink or snack has been dispensed.
